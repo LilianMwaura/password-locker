2f7e91bd138f8453649e9e9aeee4da068fc3007c
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-23 11:14:05 +0300

    create User method

--- user.py
+++ user.py
@@ -2,3 +2,17 @@ class User:
     """
     Class that generates new instances of contacts
     """
+    user_list = []
+
+    def __init__(self,user_name,password):
+        
+        self.user_name = user_name
+        self.password = password
+    
+    def save_user (self):
+
+        '''
+        save_user method saves a new user objects to the user_list
+        '''
+        User.user_list.append(self)
+        
