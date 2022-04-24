ecb59439c5087b7d9110f71b55813ff28c06c477
LilianMwaura <wambuimwaura28@gmail.com>
2022-04-24 12:36:47 +0300

    create user fuction

--- run.py
+++ run.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3.9
+
+from user import User
+def create_user(user_name,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(user_name,password)
+    return new_user
+
+def save_users(user):
+    '''
+    Function to save user
+    '''
+    contact.save_user()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    contact.delete_contact()
